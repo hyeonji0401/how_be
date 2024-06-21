8326ad7aec9192b7bdfbcb0cc3bd0e1acac04dcc
Doyeong Lee <09jjrr21@naver.com>
2024-06-21 21:21:41 +0900

    feat:기본

---
+++ src/test/java/HOW/how/HowApplicationTests.java
@@ -0,0 +1,13 @@
+package HOW.how;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
