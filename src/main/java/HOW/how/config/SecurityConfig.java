65c78ad9a79c654b085f8401636aa3ea19b2dcb7
ghdgu <ghdguswl13@gmail.com>
2024-07-09 10:00:39 +0900

    feat: SecurityConfig 작성

---
+++ src/main/java/HOW/how/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package HOW.how.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf((auth)->auth.disable());
+        http
+                .formLogin((auth)->auth.disable());
+        http
+                .httpBasic((auth)->auth.disable());
+        http
+                .authorizeHttpRequests((auth)->auth
+                        .requestMatchers("/", "member/login", "member/join").permitAll()
+                        .anyRequest().authenticated());
+        http
+                .sessionManagement((session)->session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+}
