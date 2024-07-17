e5ab1f9eb300679bca6f39f4ba4df9201fd11ebc
ghdgu <ghdguswl13@gmail.com>
2024-07-18 00:37:28 +0900

    fix: 다대다 매핑으로 인한 hashStackoverflow해결

--- src/main/java/HOW/how/domain/Liked.java
+++ src/main/java/HOW/how/domain/Liked.java
@@ -1,11 +1,14 @@
 package HOW.how.domain;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Data
+@Getter
+@Setter
 @Document(collection = "liked")
 public class Liked {
     @Id
